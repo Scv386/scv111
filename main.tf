179f73d1ce46b30024db59b9d158ebce195c7e23
Scv386 <151960757+Scv386@users.noreply.github.com>
2025-10-31 10:39:07 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,19 @@
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_resource_group" "rg" {
-  name     = "aks-project-rg"
-  location = "northeurope"
-}
-
-resource "azurerm_kubernetes_cluster" "aks" {
+resource "azurerm_kubernetes_cluster" "aks" 
+  {
   name                = "aks-cluster"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = "aksdemo"
+  location            = azurerm_resource_group.aks.location
+  resource_group_name = azurerm_resource_group.aks.name
+  dns_prefix          = "aks-cluster"
+  
   default_node_pool {
-    name       = "nodepool1"
-    node_count = 2
-    vm_size    = "Standard_B4ms"
+    name                = "nodepool1"
+    node_count          = 2
+    vm_size             = "Standard_DS2_v2"
+    enable_auto_scaling = true
+    min_count           = 1
+    max_count           = 5
   }
+  
   identity {
     type = "SystemAssigned"
   }
